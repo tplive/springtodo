e26f9b3626974af183dd83b9a2713919434dc40a
Thomas Qvidahl <tp@live.no>
2017-10-29 11:51:59 +0100

    Lagt til logging. Manglet Configuration annotation på klassen. Manglet Bean annotation på UserDetailsService. Manglet init av auth med userDetailService i init metoden.

--- src/main/java/no/qvidahl/springtodo/WebSecurityConfiguration.java
+++ src/main/java/no/qvidahl/springtodo/WebSecurityConfiguration.java
@@ -2,7 +2,10 @@ package no.qvidahl.springtodo;
 
 import no.qvidahl.springtodo.model.Bruker;
 import no.qvidahl.springtodo.model.BrukerRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -11,16 +14,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Configuration
 public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    private static final Logger log = Logger.getLogger(TodoController.class);
 
     @Autowired
     BrukerRepository brukerRepository;
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
-        super.init(auth);
+        auth.userDetailsService(userDetailsService());
     }
 
+    @Bean
     UserDetailsService userDetailsService() {
 
         return new UserDetailsService() {
@@ -28,6 +35,7 @@ public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdap
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
                 Bruker bruker = brukerRepository.findByUsername(username);
+                log.info(bruker.getUsername());
 
                 if(bruker != null) {
 
